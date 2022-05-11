92e4272f4b2d1261c4d699ffa0c3a3ecac024f15
JertixOne <84728305+JertixOne@users.noreply.github.com>
2022-05-11 16:16:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "fun.h"
 
 int main() {
-   const char* str = "Hello dm1t7ri a am Yo4u Fat2her my dear s0n";
+  const char* str = "Hello dm1t7ri a am Yo4u Fat2her my dear s0n";
     unsigned int result1 = faStr1(str);
     unsigned int result2 = faStr2(str);
     unsigned int result3 = faStr3(str);
